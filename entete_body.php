23ed6a1bc755fc0703f1c1be26bd0963d9cf807e
diafanta <diakitefanta14@yahoo.fr>
2016-05-27 18:55:29 +0000

    Add files via upload

---
+++ entete_body.php
@@ -0,0 +1,95 @@
+<header id="header"><!--header-->
+		<div class="header_top"><!--header_top-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="contactinfo">
+							<ul class="nav nav-pills">
+								<li><a href="#"><i class="fa fa-phone"></i> +237 694 847 037</a></li>
+								<li><a href="#"><i class="fa fa-envelope"></i> fdiakite14@gmail.com</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header_top-->
+		
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="index.html"><img src="images/home/logo.png" alt="" /></a>
+						</div>
+						
+					</div>
+					<div class="col-sm-8">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fa fa-user"></i> Compte</a></li>
+								<li><a href="#"><i class="fa fa-star"></i> En Promotion</a></li>
+								
+								<li><a href="cart.php"><i class="fa fa-shopping-cart"></i> Panier</a></li>
+								<li><a href="login.php"><i class="fa fa-lock"></i> Connexion</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+	
+		<div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="index.html" class="active">Accueil</a></li>
+								<li class="dropdown"><a href="#">Bagues<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="jonc.php">Jonc</a></li>
+										<li><a href="solitaire.php">Solitaire</a></li> 
+										<li><a href="alliance.php">Alliance</a></li> 
+										<li><a href="chevalière.php">Chevalière</a></li> 
+										 
+                                    </ul>
+                                </li> 
+								<li class="dropdown"><a href="#">Bracelets<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="bracelets_perle.php">Bracelets de perle</a></li>
+										<li><a href="bracelets_gourmette.php">Bracelets gourmettes</a></li>
+										<li><a href="bracelets_jonc.php">Bracelets jonc</a></li>
+										<li><a href="bracelets_macrame.php">Bracelets macramé</a></li>
+										<li><a href="bracelets_manchette.php">Bracelets manchette</a></li>
+                                    </ul>
+                                </li> 
+
+                                <li><a href="Boucle_oreille.php">Boucle d'oreille</a></li>
+								<li><a href="collier.php">Colliers</a></li>
+								<li><a href="chaine.php">Chaines</a></li>
+								<li><a href="médaillon.php">Médaillons</a></li>
+								<li><a href="pendentifs.php">Pendentifs</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="search_box pull-right">
+							<input type="text" placeholder="Rechercher"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-bottom-->
+	</header><!--/header-->
---
+++ menus.php
@@ -0,0 +1,90 @@
+<div class="col-sm-3">
+					<div class="left-sidebar">
+						<h2>Categorie</h2>
+						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Couleur
+										</a>
+									</h4>
+								</div>
+								<div id="sportswear" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Or </a></li>
+											<li><a href="#"> Argent</a></li>
+											<li><a href="#">Bleu </a></li>
+											<li><a href="#">Vert</a></li>
+											<li><a href="#">Orange </a></li>
+											<li><a href="#">jaune </a></li>
+											<li><a href="#">Rose </a></li>
+											<li><a href="#">Vert turquoi </a></li>
+											<li><a href="#">Bronze </a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#mens">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Genre
+										</a>
+									</h4>
+								</div>
+								<div id="mens" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Homme</a></li>
+											<li><a href="#">femme</a></li>
+											
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#sportswear1">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Evènement
+										</a>
+									</h4>
+								</div>
+								<div id="sportswear1" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Mariage</a></li>
+											<li><a href="#">Anniversaire</a></li>
+											<li><a href="#">Saint valentin</a></li>
+											<li><a href="#">Pac</a></li>
+											<li><a href="#">Noel</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+						
+							
+						</div><!--/category-products-->
+					
+						
+						
+						<div class="price-range"><!--price-range-->
+							<h2>Price Range</h2>
+							<div class="well text-center">
+								 <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2" ><br />
+								 <b class="pull-left">$ 0</b> <b class="pull-right">$ 600</b>
+							</div>
+						</div><!--/price-range-->
+						
+						<!--<div class="shipping text-center"><!--shipping 
+							<img src="images/home/shipping.jpg" alt="" />
+						</div>/shipping-->
+					
+					</div>
+				</div>
